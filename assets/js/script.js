749ece8da6b517d44cc969d05577b8fbd0ee0252
lucynowland <>
2023-06-22 11:33:27 +0000

    Add the questions & answers to the JS structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,63 @@
 const questions = [
     {
-        question: "",
+        question: "Which of the following would describe you the best?",
         answers: [
-           {text: "", house: gryffindor},
-           {text: "", house: ravenclaw},
-           {text: "" house: hufflepuff},
-           {text: "" house: slytherin},
+           {text: "Brave", house: gryffindor},
+           {text: "Intelligent", house: ravenclaw},
+           {text: "Caring" house: hufflepuff},
+           {text: "Cunning" house: slytherin},
         ],
    
    },
    {
-    question: "",
+    question: "Which of the following subjects would you most like to study at Hogwarts?",
     answers: [
-       {text: "", house: gryffindor},
-       {text: "", house: ravenclaw},
-       {text: "" house: hufflepuff},
-       {text: "" house: slytherin},
+       {text: "Transfiguration", house: gryffindor},
+       {text: "Charms", house: ravenclaw},
+       {text: "Care of Magical Creatures" house: hufflepuff},
+       {text: "Potions" house: slytherin},
     ],
 
     },
     {
-        question: "",
+        question: "As a 1st year student, which animal would you most like to accompany you to Hogwarts?",
         answers: [
-           {text: "", house: gryffindor},
-           {text: "", house: ravenclaw},
-           {text: "" house: hufflepuff},
-           {text: "" house: slytherin},
+           {text: "A rat", house: gryffindor},
+           {text: "An owl", house: ravenclaw},
+           {text: "A toad" house: hufflepuff},
+           {text: "A cat" house: slytherin},
         ],
    
     },
     {
-        question: "",
+        question: "What career path would entice you the most once you had completed your years at Hogwarts?",
         answers: [
-           {text: "", house: gryffindor},
-           {text: "", house: ravenclaw},
-           {text: "" house: hufflepuff},
-           {text: "" house: slytherin},
+           {text: "An auror", house: gryffindor},
+           {text: "A professor", house: ravenclaw},
+           {text: "A healer" house: hufflepuff},
+           {text: "An alchemist" house: slytherin},
         ],
    
     },
     {
-        question: "",
+        question: "What is your Zodiac sign?",
         answers: [
-           {text: "", house: gryffindor},
-           {text: "", house: ravenclaw},
-           {text: "" house: hufflepuff},
-           {text: "" house: slytherin},
+           {text: "Aries, Leo and Sagittarus", house: gryffindor},
+           {text: "Gemini, Libra and Aquarius", house: ravenclaw},
+           {text: "Taurus, Virgo and Capricorn" house: hufflepuff},
+           {text: "Cancer, Scorpio & Pisces" house: slytherin},
         ],
    
     },
     {
-        question: "",
+        question: "Yay! You've just become a member of the Quidditch team, what position are you playing?",
         answers: [
-           {text: "", house: gryffindor},
-           {text: "", house: ravenclaw},
-           {text: "" house: hufflepuff},
-           {text: "" house: slytherin},
+           {text: "Seeker", house: gryffindor},
+           {text: "Chaser", house: ravenclaw},
+           {text: "Crowd supporter" house: hufflepuff},
+           {text: "Beater" house: slytherin},
         ],
    
     },
-    {
-        question: "",
-        answers: [
-           {text: "", house: gryffindor},
-           {text: "", house: ravenclaw},
-           {text: "" house: hufflepuff},
-           {text: "" house: slytherin},
-        ],
-   
-    },
-    {
-        question: "",
-        answers: [
-           {text: "", house: gryffindor},
-           {text: "", house: ravenclaw},
-           {text: "" house: hufflepuff},
-           {text: "" house: slytherin},
-        ],
-   
-    },
-    {
-        question: "",
-        answers: [
-           {text: "", house: gryffindor},
-           {text: "", house: ravenclaw},
-           {text: "" house: hufflepuff},
-           {text: "" house: slytherin},
-        ],
-   
-    },
-    {
-        question: "",
-        answers: [
-           {text: "", house: gryffindor},
-           {text: "", house: ravenclaw},
-           {text: "" house: hufflepuff},
-           {text: "" house: slytherin},
-        ],
-   
-    },
+    
 ]
