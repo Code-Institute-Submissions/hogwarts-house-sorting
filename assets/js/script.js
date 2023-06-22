73a59a4696c5d7a841cce83d480b98aac406187b
lucynowland <>
2023-06-22 11:08:14 +0000

    Add basic JS code structure for questions

---
+++ assets/js/script.js
@@ -0,0 +1,102 @@
+const questions = [
+    {
+        question: "",
+        answers: [
+           {text: "", house: gryffindor},
+           {text: "", house: ravenclaw},
+           {text: "" house: hufflepuff},
+           {text: "" house: slytherin},
+        ],
+   
+   },
+   {
+    question: "",
+    answers: [
+       {text: "", house: gryffindor},
+       {text: "", house: ravenclaw},
+       {text: "" house: hufflepuff},
+       {text: "" house: slytherin},
+    ],
+
+    },
+    {
+        question: "",
+        answers: [
+           {text: "", house: gryffindor},
+           {text: "", house: ravenclaw},
+           {text: "" house: hufflepuff},
+           {text: "" house: slytherin},
+        ],
+   
+    },
+    {
+        question: "",
+        answers: [
+           {text: "", house: gryffindor},
+           {text: "", house: ravenclaw},
+           {text: "" house: hufflepuff},
+           {text: "" house: slytherin},
+        ],
+   
+    },
+    {
+        question: "",
+        answers: [
+           {text: "", house: gryffindor},
+           {text: "", house: ravenclaw},
+           {text: "" house: hufflepuff},
+           {text: "" house: slytherin},
+        ],
+   
+    },
+    {
+        question: "",
+        answers: [
+           {text: "", house: gryffindor},
+           {text: "", house: ravenclaw},
+           {text: "" house: hufflepuff},
+           {text: "" house: slytherin},
+        ],
+   
+    },
+    {
+        question: "",
+        answers: [
+           {text: "", house: gryffindor},
+           {text: "", house: ravenclaw},
+           {text: "" house: hufflepuff},
+           {text: "" house: slytherin},
+        ],
+   
+    },
+    {
+        question: "",
+        answers: [
+           {text: "", house: gryffindor},
+           {text: "", house: ravenclaw},
+           {text: "" house: hufflepuff},
+           {text: "" house: slytherin},
+        ],
+   
+    },
+    {
+        question: "",
+        answers: [
+           {text: "", house: gryffindor},
+           {text: "", house: ravenclaw},
+           {text: "" house: hufflepuff},
+           {text: "" house: slytherin},
+        ],
+   
+    },
+    {
+        question: "",
+        answers: [
+           {text: "", house: gryffindor},
+           {text: "", house: ravenclaw},
+           {text: "" house: hufflepuff},
+           {text: "" house: slytherin},
+        ],
+   
+    },
+]
