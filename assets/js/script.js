012e95b3f58242b963f22145412b8e257ddea9c0
lucynowland <>
2023-06-22 23:35:27 +0000

    Supply corresponding house answers to dataset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//*Quiz questions and answer options
+
 const quizData = [
     {
         question: "Which of the following would describe you the best?",
@@ -66,3 +68,91 @@ const quizData = [
         slytherinAnswer: "d",
     },
 ];
+
+//* Get elements from DOM 
+const quiz= document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+//* Data once quiz is loaded or restarted
+let currentQuiz = 0;
+let gryffindorHouse = 0;
+let ravenclawHouse = 0;
+let hufflepuffHouse = 0;
+let slytherinHouse = 0;
+
+loadQuiz()
+
+/**
+ * Loads each question & set of answer options for each index
+ */
+function loadQuiz() {
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuiz]
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+        if(answer === quizData[currentQuiz].gryffindorAnswer) {
+            gryffindorHouse++
+        }
+        if(answer === quizData[currentQuiz].ravenclawAnswer) {
+            ravenclawHouse++
+        }
+        if(answer === quizData[currentQuiz].hufflepuffAnswer) {
+            hufflepuffHouse++
+        }
+        if(answer === quizData[currentQuiz].slytherinAnswer) {
+            House++
+        }
+        currentQuiz++
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else if (gryffindorHouse >= quizData.length){
+            quiz.innerHTML = `
+            <h2>You are Gryffindor!</h2>
+            <button onclick="location.reload()">Restart</button>
+            `
+        } else if (ravenclawHouse >= quizData.length){
+            quiz.innerHTML = `
+            <h2>You are Ravenclaw!</h2>
+            <button onclick="location.reload()">Restart</button>
+            `
+        } else if (hufflepuffHouse >= quizData.length){
+            quiz.innerHTML = `
+            <h2>You are Hufflepuff!</h2>
+            <button onclick="location.reload()">Restart</button>
+            `
+        } else if (slytherinHouse >= quizData.length){
+            quiz.innerHTML = `
+            <h2>You are Slytherin!</h2>
+            <button onclick="location.reload()">Restart</button>
+            `
+        }
+     }
+ })
