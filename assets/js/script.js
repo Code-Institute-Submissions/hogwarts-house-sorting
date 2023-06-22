7d04c9f88d7e377bc44a72ed5e161ee313fad65c
lucynowland <>
2023-06-22 17:36:59 +0000

    Add new JS structure for question & answer options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,52 @@
-const questions = [
+const quizData = [
     {
         question: "Which of the following would describe you the best?",
-        answers: [
-           {text: "Brave", correct: true},
-           {text: "Intelligent", correct: true},
-           {text: "Caring", correct: true},
-           {text: "Cunning", correct: true},
-        ],
-   },
-   {
-       question: "Which of the following subjects would you most like to study at Hogwarts?",
-       answers: [
-        {text: "Transfiguration", correct: true},
-        {text: "Charms", correct: true},
-        {text: "Care of Magical Creatures", correct: true},
-        {text: "Potions", correct: true},
-        ], 
-
+        a: "Brave",
+        b: "Intelligent",
+        c: "Caring",
+        d: "Cunning",
+    },
+    {
+        question: "Which of the following subjects would you most like to study at Hogwarts?",
+        a: "Transfiguration",
+        b: "Charms",
+        c: "Care of Magical Creatures",
+        d: "Potions",
     },
     {
         question: "As a 1st year student, which animal would you most like to accompany you to Hogwarts?",
-        answers: [
-           {text: "A rat", correct: true},
-           {text: "An owl", correct: true},
-           {text: "A toad", correct: true},
-           {text: "A cat", correct: true},
-        ],
-   
+        a: "A rat",
+        b: "An owl",
+        c: "A toad",
+        d: "A cat",
     },
     {
         question: "What career path would entice you the most once you had completed your years at Hogwarts?",
-        answers: [
-           {text: "An auror", correct: true},
-           {text: "A professor", correct: true},
-           {text: "A healer", correct: true},
-           {text: "An alchemist", correct: true},
-        ],
-   
+        a: "An auror",
+        b: "A professor",
+        c: "A healer",
+        d: "An alchemist",
     },
     {
         question: "What is your Zodiac sign?",
-        answers: [
-           {text: "Aries, Leo and Sagittarus", correct: true},
-           {text: "Gemini, Libra and Aquarius", correct: true},
-           {text: "Taurus, Virgo and Capricorn", correct: true},
-           {text: "Cancer, Scorpio & Pisces", correct: true},
-        ],
-   
+        a: "Aries, Leo and Sagittarus",
+        b: "Gemini, Libra and Aquarius",
+        c: "Taurus, Virgo and Capricorn",
+        d: "Cancer, Scorpio & Pisces",
     },
     {
         question: "Yay! You've just become a member of the Quidditch team, what position are you playing?",
-        answers: [
-           {text: "Seeker", correct: true},
-           {text: "Chaser", correct: true},
-           {text: "Crowd supporter", correct: true},
-           {text: "Beater", correct: true},
-        ],
-   
-    },
-    
+        a: "Seeker",
+        b: "Chaser",
+        c: "Crowd supporter",
+        d: "Beater"
+    }
+
 ];
 
+
+   
+    
 
 
 
